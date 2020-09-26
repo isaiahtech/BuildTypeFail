0448ad5d1d5f18e1ad679e46194fe9d4acc59be3
Isaiah <dothackdasen@gmail.com>
2020-09-26 15:10:22 -0700

    casting types

--- BuildTypeFail/Program.cs
+++ BuildTypeFail/Program.cs
@@ -6,9 +6,37 @@ namespace BuildTypeFail
     {
         static void Main(string[] args)
         {
-            float myFloatValue = 10;
-            int myIntValue = (int) myFloatValue;
-            Console.WriteLine("myIntValue is " + myIntValue);
+            int myInt = 10;
+
+            byte myByte = (byte)myInt;
+
+            double myDouble = (double)myByte;
+
+            // bool myBool = (bool)myDouble;
+
+            string myString = "false";
+
+            // myBool = (bool)myString;
+
+            // myString = (string)myInt;
+
+            myString = myInt.ToString();
+
+            // myBool = (bool)myByte;
+
+            // myByte = (byte)myBool;
+
+            short myShort = (short)myInt;
+
+            char myChar = 'x';
+
+            // myString = (string)myChar;
+
+            long myLong = (long)myInt;
+
+            decimal myDecimal = (decimal)myLong;
+
+            myString = myString + myInt + myByte + myDouble + myChar;
         }
     }
 }
