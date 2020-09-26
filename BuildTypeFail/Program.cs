a2208f2a1f481c8fa32ca9ebb990dae4111c74fc
Isaiah <dothackdasen@gmail.com>
2020-09-25 23:24:13 -0700

    Add project files.

---
+++ BuildTypeFail/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace BuildTypeFail
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float myFloatValue = 10;
+            int myIntValue = (int) myFloatValue;
+            Console.WriteLine("myIntValue is " + myIntValue);
+        }
+    }
+}
